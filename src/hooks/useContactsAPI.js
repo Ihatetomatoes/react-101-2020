dd33af55acaf87c8bab9365d887b6e67fc015acd
Petr Tichy <peter@ihatetomatoes.net>
2020-05-23 18:17:25 +1000

    export fetchUsers fn from hook

--- src/hooks/useContactsAPI.js
+++ src/hooks/useContactsAPI.js
@@ -7,29 +7,31 @@ const useContactsAPI = () => {
     const [isError, setError] = useState(false);
 
     useEffect(() => {
-
-        setLoading(true);
-        fetch('https://randomuser.me/api/?results=20')
-        .then(response => response.json())
-        .then(data => data.results.map(user => (
-            {
-                title: `${user.name.first} ${user.name.last}`,
-                content: `${user.login.username}`
-            }
-        )))
-        .then(users => {
-            setUsers(users);
-            setLoading(false);
-        })
-        .catch(error => {
-            console.log(error);
-            setLoading(false);
-            setError(true);
-        });
-
+        fetchUsers();
     }, []);
 
-    return [{users, isLoading, isError}];
+    const fetchUsers = async () => {
+        setLoading(true);
+        await fetch('https://randomuser.me/api/?results=20')
+            .then(response => response.json())
+            .then(data => data.results.map(user => (
+                {
+                    title: `${user.name.first} ${user.name.last}`,
+                    content: `${user.login.username}`
+                }
+            )))
+            .then(users => {
+                setUsers(users);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setLoading(false);
+                setError(true);
+            });
+    }
+
+    return [{users, isLoading, isError}, fetchUsers];
 
 }
 
